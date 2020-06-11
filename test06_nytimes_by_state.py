cc62fc806de46046899e768610a579bf6908db72
Jay Flatland <jayflatland@gmail.com>
2020-06-11 12:50:35 -0500

    Save

--- test06_nytimes_by_state.py
+++ test06_nytimes_by_state.py
@@ -15,147 +15,154 @@ df = pd.read_csv('../../Opensource/covid-19-data/us-states.csv')
 df['date'] = pd.to_datetime(df['date'])
 # df = df.set_index(pd.to_datetime(df['date'])).sort_index()
 
-# %%
-
-r, c = 1, 2
-fig, axs = plt.subplots(r, c, figsize=(18, 18), sharex=True)
-if r == 1 and c == 1: axs = [axs]
-elif r == 1 or c == 1:  axs = list(axs)
-else: axs = [axs[i, j] for j in range(c) for i in range(r)]  # flatten
-
-
-d = df.pivot_table(index='date', columns='state', values='cases')
-d['Total'] = d.sum(axis=1)
-dd = d.diff()
-dr = dd / d * 100.0
-
-ddr = (dd / dd.shift(1) - 1) * 100.0
-
-cols = [
-    'New York',
-    'California',
-    #'Michigan',
-    #'Kansas',
-    #'Missouri',
-    'Texas',
-    'Florida',
-    'Total',
-]
-
-#plt.sca(axs.pop(0))
-#plt.title("Cases")
-#
-#for c in cols:
-#    plt.plot(d[c].rolling(3).mean(), label=c)
-#    # plt.plot(dr[c], label=c)
-#plt.legend()
-#plt.xticks(rotation=45)
-#plt.subplots_adjust(bottom=0.15)
-
-
-#plt.sca(axs.pop(0))
-#plt.title("Daily Cases Increases")
-#
-#for c in cols:
-#    plt.plot(dd[c].rolling(3).mean(), label=c)
-#    # plt.plot(dr[c], label=c)
-#plt.legend()
-#plt.xticks(rotation=45)
-#plt.subplots_adjust(bottom=0.15)
-
-
-
-plt.sca(axs.pop(0))
-plt.title("Daily Cases Increase Percent (3 day MA)")
-
-for c in cols:
-    plt.plot(dr[c].rolling(3).mean(), label=c)
-    # plt.plot(dr[c], label=c)
-plt.legend()
-plt.xticks(rotation=45)
-plt.subplots_adjust(bottom=0.15)
-
-
-
-# plt.sca(axs.pop(0))
-# plt.title("Daily New Cases Percent Change")# (3 day MA)")
-
-# for c in cols:
-#     # plt.plot(ddr[c].rolling(3).mean(), label=c)
-#     plt.plot(ddr[c], label=c)
-# plt.legend()
-# plt.xticks(rotation=45)
-# plt.subplots_adjust(bottom=0.15)
 
 # %%
-
-d = df.pivot_table(index='date', columns='state', values='deaths')
-d['Total'] = d.sum(axis=1)
-dd = d.diff()
-dr = dd / d * 100.0
-
-cols = [
-    'New York',
-    'California',
-    # 'Michigan',
-    # 'Kansas',
-    # 'Missouri',
-    # 'Texas',
-    # 'Florida',
-    'Total',
-]
-
-#plt.sca(axs.pop(0))
-#plt.title("Deaths")
-#
-#for c in cols:
-#    plt.plot(d[c].rolling(3).mean(), label=c)
-#    # plt.plot(dr[c], label=c)
-#plt.legend()
-#plt.xticks(rotation=45)
-#plt.subplots_adjust(bottom=0.15)
-
-
-#plt.sca(axs.pop(0))
-#plt.title("Daily Deaths Increases")
-#
-#for c in cols:
-#    plt.plot(dd[c].rolling(3).mean(), label=c)
-#    # plt.plot(dr[c], label=c)
-#plt.legend()
-#plt.xticks(rotation=45)
-#plt.subplots_adjust(bottom=0.15)
-
-
-# plt.sca(axs.pop(0))
-
-
-plt.sca(axs.pop(0))
-plt.title("Daily Deaths Increase Percent (3 day MA)")
-
-for c in cols:
-    plt.plot(dr[c].rolling(3).mean(), label=c)
-    # plt.plot(dr[c], label=c)
-plt.legend()
-plt.xticks(rotation=45)
-plt.subplots_adjust(bottom=0.15)
-
-
-plt.show()
+d = df.pivot_table(index='date', values='cases', columns='state')
+d = d.fillna(0.0)
+d = d.diff()
+d = d.rolling(3).mean()
 
 # %%
-
-#plt.figure()
-#plt.plot(d['New York'], dd['New York'])
-
-## %%
-#plt.figure()
-#plt.plot(d['Total'], dd['Total'])
-#
-## %%
-#plt.figure()
-#plt.plot(dl['New York'].diff())
-#
-## %%
-#plt.figure()
-#plt.plot(dl['Total'].diff())
+if 0:
+    r, c = 1, 2
+    fig, axs = plt.subplots(r, c, figsize=(18, 18), sharex=True)
+    if r == 1 and c == 1: axs = [axs]
+    elif r == 1 or c == 1:  axs = list(axs)
+    else: axs = [axs[i, j] for j in range(c) for i in range(r)]  # flatten
+    
+    
+    d = df.pivot_table(index='date', columns='state', values='cases')
+    d['Total'] = d.sum(axis=1)
+    dd = d.diff()
+    dr = dd / d * 100.0
+    
+    ddr = (dd / dd.shift(1) - 1) * 100.0
+    
+    cols = [
+        'New York',
+        'California',
+        #'Michigan',
+        #'Kansas',
+        #'Missouri',
+        'Texas',
+        'Florida',
+        'Total',
+    ]
+    
+    #plt.sca(axs.pop(0))
+    #plt.title("Cases")
+    #
+    #for c in cols:
+    #    plt.plot(d[c].rolling(3).mean(), label=c)
+    #    # plt.plot(dr[c], label=c)
+    #plt.legend()
+    #plt.xticks(rotation=45)
+    #plt.subplots_adjust(bottom=0.15)
+    
+    
+    #plt.sca(axs.pop(0))
+    #plt.title("Daily Cases Increases")
+    #
+    #for c in cols:
+    #    plt.plot(dd[c].rolling(3).mean(), label=c)
+    #    # plt.plot(dr[c], label=c)
+    #plt.legend()
+    #plt.xticks(rotation=45)
+    #plt.subplots_adjust(bottom=0.15)
+    
+    
+    
+    plt.sca(axs.pop(0))
+    plt.title("Daily Cases Increase Percent (3 day MA)")
+    
+    for c in cols:
+        plt.plot(dr[c].rolling(3).mean(), label=c)
+        # plt.plot(dr[c], label=c)
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.subplots_adjust(bottom=0.15)
+    
+    
+    
+    # plt.sca(axs.pop(0))
+    # plt.title("Daily New Cases Percent Change")# (3 day MA)")
+    
+    # for c in cols:
+    #     # plt.plot(ddr[c].rolling(3).mean(), label=c)
+    #     plt.plot(ddr[c], label=c)
+    # plt.legend()
+    # plt.xticks(rotation=45)
+    # plt.subplots_adjust(bottom=0.15)
+    
+    # %%
+    
+    d = df.pivot_table(index='date', columns='state', values='deaths')
+    d['Total'] = d.sum(axis=1)
+    dd = d.diff()
+    dr = dd / d * 100.0
+    
+    cols = [
+        'New York',
+        'California',
+        # 'Michigan',
+        # 'Kansas',
+        # 'Missouri',
+        # 'Texas',
+        # 'Florida',
+        'Total',
+    ]
+    
+    #plt.sca(axs.pop(0))
+    #plt.title("Deaths")
+    #
+    #for c in cols:
+    #    plt.plot(d[c].rolling(3).mean(), label=c)
+    #    # plt.plot(dr[c], label=c)
+    #plt.legend()
+    #plt.xticks(rotation=45)
+    #plt.subplots_adjust(bottom=0.15)
+    
+    
+    #plt.sca(axs.pop(0))
+    #plt.title("Daily Deaths Increases")
+    #
+    #for c in cols:
+    #    plt.plot(dd[c].rolling(3).mean(), label=c)
+    #    # plt.plot(dr[c], label=c)
+    #plt.legend()
+    #plt.xticks(rotation=45)
+    #plt.subplots_adjust(bottom=0.15)
+    
+    
+    # plt.sca(axs.pop(0))
+    
+    
+    plt.sca(axs.pop(0))
+    plt.title("Daily Deaths Increase Percent (3 day MA)")
+    
+    for c in cols:
+        plt.plot(dr[c].rolling(3).mean(), label=c)
+        # plt.plot(dr[c], label=c)
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.subplots_adjust(bottom=0.15)
+    
+    
+    plt.show()
+    
+    # %%
+    
+    #plt.figure()
+    #plt.plot(d['New York'], dd['New York'])
+    
+    ## %%
+    #plt.figure()
+    #plt.plot(d['Total'], dd['Total'])
+    #
+    ## %%
+    #plt.figure()
+    #plt.plot(dl['New York'].diff())
+    #
+    ## %%
+    #plt.figure()
+    #plt.plot(dl['Total'].diff())
