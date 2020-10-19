7f9a888f6b98bac112a34bd03808621d1cd08508
Jay Flatland <jayflatland@gmail.com>
2020-10-19 10:50:12 -0500

    Save

--- test07_counties.py
+++ test07_counties.py
@@ -12,15 +12,25 @@ df = pd.read_csv("../../Opensource/COVID-19/csse_covid_19_data/csse_covid_19_tim
 #df = df[df['Country_Region'] == 'US']
 #df = df[df['Province/State'] == 'Johnson County, KS']
 df = df[df['Combined_Key'] == 'Mille Lacs, Minnesota, US']
+#df = df[df['Combined_Key'] == 'Stearns, Minnesota, US']
+#df = df[df['Combined_Key'] == 'Steele, Minnesota, US']
+
 #Mille Lacs, Minnesota, US
 
 #df = df.set_index('Province/State')
 df = df[df.columns[11:]].T
+df = df.set_index(pd.to_datetime(df.index))
+df.columns = ['v']
 df['d'] = df.diff()
 df['u'] = df['d'].rolling(7).mean()
 
 # %%
-plt.plot(df.diff().rolling(7).mean())
+plt.figure()
+plt.title("Mille Lacs County Covid Cases")
+plt.plot(df['d'], label='Daily New Cases')
+plt.plot(df['u'], label='7 Day Moving Average')
+plt.legend()
+plt.show()
 
 # dfs = []
 # # from https://github.com/CSSEGISandData/COVID-19.git
@@ -41,11 +51,11 @@ plt.plot(df.diff().rolling(7).mean())
 
 
 
-df = pd.read_csv("../../Opensource/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv")
-df = df[df['Province_State'] == 'Kansas']
+# df = pd.read_csv("../../Opensource/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv")
+# df = df[df['Province_State'] == 'Kansas']
 
-# %%
+# # %%
 
-df = pd.read_csv("../../Opensource/COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv")
+# df = pd.read_csv("../../Opensource/COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv")
 
-df = df.set_index(list(df.columns[:3])).T
+# df = df.set_index(list(df.columns[:3])).T
