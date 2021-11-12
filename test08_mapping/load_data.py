558217695de3809ddb0d79baab60af4df9a58bd1
Jay Flatland <jayflatland@gmail.com>
2021-11-12 08:34:55 -0600

    Work on mapping

--- test08_mapping/load_data.py
+++ test08_mapping/load_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 from pandas import Timestamp, Timedelta
 import matplotlib.pylab as plt
 import seaborn as sns
+from county_to_county_code_map import county_to_county_code_map
 
 # %%
 
@@ -15,7 +16,17 @@ deaths = pd.read_csv("../../../Opensource/COVID-19/csse_covid_19_data/csse_covid
 df = cases
 df = df.rename(columns={"Combined_Key": "County"})
 
+county_ids = pd.DataFrame(county_to_county_code_map)
+df = df[df.columns[:11]]
+df.merge(county_ids, how='left', on='County')
+
 # %%
-import xml.etree.ElementTree as ET
-tree = ET.parse('Usa_counties_large.svg')
-root = tree.getroot()
+# import xml.etree.ElementTree as ET
+# tree = ET.parse('Usa_counties_large.svg')
+# root = tree.getroot()
+
+
+# for e in root:
+#     print(e)
+
+# #root.findall('.//path')
