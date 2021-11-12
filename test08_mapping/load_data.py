8bbe36652c9495df9e4626b2a8b2764f5048a996
Jay Flatland <jayflatland@gmail.com>
2021-11-12 09:11:37 -0600

    Getting close!

--- test08_mapping/load_data.py
+++ test08_mapping/load_data.py
@@ -5,6 +5,7 @@ from pandas import Timestamp, Timedelta
 import matplotlib.pylab as plt
 import seaborn as sns
 from county_to_county_code_map import county_to_county_code_map
+import matplotlib as mpl
 
 # %%
 
@@ -12,22 +13,42 @@ cases = pd.read_csv("../../../Opensource/COVID-19/csse_covid_19_data/csse_covid_
 deaths = pd.read_csv("../../../Opensource/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv")
 
 # %%
+county_ids = pd.DataFrame(county_to_county_code_map)
+county_ids = county_ids.rename(columns={"code": "County_ID"})
+
+# %%
 
 df = cases
 df = df.rename(columns={"Combined_Key": "County"})
-
-county_ids = pd.DataFrame(county_to_county_code_map)
-#df = df[df.columns[:11]]
 df = df.merge(county_ids, how='left', on='County')
+df = df[df['County_ID'].notnull()]
+df = df.set_index('County_ID')
+df = df[df.columns[11:]].T
+df = df.set_index(pd.to_datetime(df.index))
 
-#list(cases["Province_State"].unique())
 # %%
-# import xml.etree.ElementTree as ET
-# tree = ET.parse('Usa_counties_large.svg')
-# root = tree.getroot()
+def colorFader(mix):
+    mix = min(mix, 1.0)
+    mix = max(mix, 0.0)
+    c1='white'
+    c2='red'
+    c1=np.array(mpl.colors.to_rgb(c1))
+    c2=np.array(mpl.colors.to_rgb(c2))
+    return mpl.colors.to_hex((1-mix)*c1 + mix*c2)
 
+# %%
 
-# for e in root:
-#     print(e)
+final = df.iloc[-1]
+final = final / final.max()
 
-# #root.findall('.//path')
+#plt.plot(list(final))
+
+fd = open('animator.js', 'w')
+
+print("function do_animator() {", file=fd)
+for county_id, x in final.items():
+    cc = colorFader(x)
+    print(f'    $("#{county_id}").css("fill", "{cc}");', file=fd)
+
+print("}", file=fd)
+fd.close()
